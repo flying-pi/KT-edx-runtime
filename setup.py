52084e05f212f795bc3b7ba47012609763b8e7be
yurabraiko <yurabraiko@gmail.com>
2019-06-17 13:04:20 +0300

    Started web service for runtime

---
+++ setup.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# pylint: disable=C0111,W6005,W6100
+from __future__ import absolute_import, print_function
+
+import os
+import re
+import sys
+
+from setuptools import setup
+
+
+def get_version(*file_paths):
+    """
+    Extract the version string from the file at the given relative path fragments.
+    """
+    filename = os.path.join(os.path.dirname(__file__), *file_paths)
+    version_file = open(filename).read()
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError('Unable to find version string.')
+
+
+VERSION = get_version('kt_edx_runtime', '__init__.py')
+
+if sys.argv[-1] == 'tag':
+    print("Tagging the version on github:")
+    os.system("git tag -a %s -m 'version %s'" % (VERSION, VERSION))
+    os.system("git push --tags")
+    sys.exit()
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+CHANGELOG = open(os.path.join(os.path.dirname(__file__), 'CHANGELOG.md')).read()
+
+
+def load_requirements(*requirements_paths):
+    """
+    Load all requirements from the specified requirements files.
+    Returns a list of requirement strings.
+    """
+    requirements = set()
+    for path in requirements_paths:
+        requirements.update(
+            line.split('#')[0].strip() for line in open(path).readlines()
+            if is_requirement(line.strip())
+        )
+    return list(requirements)
+
+
+def is_requirement(line):
+    """
+    Return True if the requirement line is a package requirement;
+    that is, it is not blank, a comment, a URL, or an included file.
+    """
+    return not (
+        line == '' or
+        line.startswith('-r') or
+        line.startswith('#') or
+        line.startswith('-e') or
+        line.startswith('git+')
+    )
+
+
+setup(
+    name='kt-edx-runtime',
+    version=VERSION,
+    description='Proctoring subsystem for Open edX',
+    long_description=README + '\n\n' + CHANGELOG,
+    author='flyingpi',
+    author_email='yurabraiko@gmail.com',
+    url='https://github.com/flying-pi/KT-edx-runtime.git',
+    license="AGPL 3.0",
+    zip_safe=False,
+    keywords='edX ',
+    packages=[
+        'kt_edx_runtime',
+    ],
+    include_package_data=True,
+    install_requires=load_requirements("requirements/base.txt"),
+)
